4792985ef5db14ceee776019e549302801ad42e9
tmkelly28 <tmkelly28@gmail.com>
2015-08-24 13:44:10 -0400

    Keeping game as global, reorganized DOM events under document ready

--- js/guessing_game_script.js
+++ js/guessing_game_script.js
@@ -1,14 +1,15 @@
-//  ToDo: Refactor functions so that game does not need to be a global variable
 //  ToDo: If submitGuessInput has focus, Enter should submit
 //  ToDo: Another animation for victory/failure
 
-// Declare variables to store each game, the alerts content
+// alerts dictionary - contains a dictionary of alert values for building new alerts
 var alerts = {
     type: ["alert alert-success", "alert alert-info", "alert alert-warning", "alert alert-danger"],
     exclaim: ["", "Yes! ", "Brr! ", "Yow! ", "Uh oh! ", "Oh noes! ", "Good guess! ", "You could "],
     body: ["", "You got it! It was: ", "Getting colder! Try again! You guessed: ", "Getting hotter! Take another guess! You guessed: ", "You've got one guess left! You guessed: ", "You're out of guesses! The answer was: ", "That's not valid - try guessing a whole number!", "Try guessing between 1 and 100!", "You already guessed ", "Here's a hint: the answer is higher than your guess of ", "Here's a hint: the answer is lower than your guess of ", "try guessing ", "Try starting a new game!"],
     hint: ["", " (Guess higher next time!)", " (Guess lower next time!)"]
   };
+// game global - holds a Game object
+var game;
 
 // Alert object constructor & helper function
 function Alert(type, exclaim, body, guess, hint) {
@@ -40,14 +41,6 @@ function createAlertObject(v, w, x, y, z) {
   var newAlert = new Alert(alerts.type[v], alerts.exclaim[w], alerts.body[x], y, alerts.hint[z]);
   newAlert.drawSelf();
 }
-
-// Animations
-$(document).ready(function () {
-  // Alert animation
-  $(".make-alerts").click(function () {
-    $($("#alerts").children("div")[0]).hide().slideDown();
-  });
-});
 
 // Game object constructor
 function Game(totalGuesses) {
@@ -102,59 +95,66 @@ Game.prototype.giveHint = function (guess) {
 // Creating a new game
 function startNewGame(defaultGuesses) {
   "use strict";
-  var game = new Game(defaultGuesses);
+  game = new Game(defaultGuesses);
   $("#alerts").html("");
   $("#guesses-remaining").html(game.totalGuesses);
   $("#submit-guess-input").removeAttr("disabled");
 }
-$("#difficulty-easy").click(function () {
+
+// DOM Events
+$(document).ready(function () {
+  // Selecting difficulty
+  $("#difficulty-easy").click(function () {
   "use strict";
   startNewGame(14);
-});
-$("#difficulty-medium").click(function () {
-  "use strict";
-  startNewGame(7);
-});
-$("#difficulty-hard").click(function () {
-  "use strict";
-  startNewGame(2);
-});
-
-// Validating a guess
-$("#submit-guess-button").click(function () {
-  "use strict";
-  var guess = Number($("#submit-guess-input").val());
-  if ($("#submit-guess-input").attr("disabled")) {
-    // case when new game hasn't been started
-    createAlertObject(2, 0, 12, "", 0);
-  } else if (isNaN(guess)) {
-    // case when guess is not a number
-    createAlertObject(2, 4, 6, "", 0);
-  } else if (guess % 1 !== 0) {
-    //case when guess is not a whole number
-    createAlertObject(2, 4, 6, "", 0);
-  } else if (guess < 1 || guess > 100) {
-    // case when guess is less than 1 or greater than 100
-    createAlertObject(2, 4, 7, "", 0);
-  } else if (game.pastGuesses.indexOf(guess) !== -1) {
-    // case when guess has already been guessed
-    createAlertObject(2, 4, 8, guess, 0);
-  } else {
-    // case when guess is valid
-    game.currentGuess = guess;
-    game.makeGuess(game.currentGuess);
-    game.pastGuesses.push(guess);
-  }
-});
-
-// Getting a hint
-$("#hint-button").click( function () {
-  "use strict";
-  if ($("#submit-guess-input").attr("disabled")) {
-    // if no game in progress, the hint is to start a new game :)
-    createAlertObject(2, 0, 12, "", 0);
-  } else {
-    // create a alert with a hint
-    createAlertObject(2, 7, 11, game.target, 0);
-  }
+  });
+  $("#difficulty-medium").click(function () {
+    "use strict";
+    startNewGame(7);
+  });
+  $("#difficulty-hard").click(function () {
+    "use strict";
+    startNewGame(2);
+  });
+  // Alert animation
+  $(".make-alerts").click(function () {
+    $($("#alerts").children("div")[0]).hide().slideDown();
+  });
+  // Validating a guess
+  $("#submit-guess-button").click(function () {
+    "use strict";
+    var guess = Number($("#submit-guess-input").val());
+    if ($("#submit-guess-input").attr("disabled")) {
+      // case when new game hasn't been started
+      createAlertObject(2, 0, 12, "", 0);
+    } else if (isNaN(guess)) {
+      // case when guess is not a number
+      createAlertObject(2, 4, 6, "", 0);
+    } else if (guess % 1 !== 0) {
+      //case when guess is not a whole number
+      createAlertObject(2, 4, 6, "", 0);
+    } else if (guess < 1 || guess > 100) {
+      // case when guess is less than 1 or greater than 100
+      createAlertObject(2, 4, 7, "", 0);
+    } else if (game.pastGuesses.indexOf(guess) !== -1) {
+      // case when guess has already been guessed
+      createAlertObject(2, 4, 8, guess, 0);
+    } else {
+      // case when guess is valid
+      game.currentGuess = guess;
+      game.makeGuess(game.currentGuess);
+      game.pastGuesses.push(guess);
+    }
+  });
+  // Getting a hint
+  $("#hint-button").click( function () {
+    "use strict";
+    if ($("#submit-guess-input").attr("disabled")) {
+      // if no game in progress, the hint is to start a new game :)
+      createAlertObject(2, 0, 12, "", 0);
+    } else {
+      // create a alert with a hint
+      createAlertObject(2, 7, 11, game.target, 0);
+    }
+  });
 });
